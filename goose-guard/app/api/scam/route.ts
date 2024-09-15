379fcc1f4b9ef84bad2d6bbfc44852c7df398637
Faisal Fawad <faisalfawad2004@gmail.com>
2024-09-15 06:43:54 -0400

    bugfix

--- goose-guard/app/api/scam/route.ts
+++ goose-guard/app/api/scam/route.ts
@@ -4,7 +4,11 @@ import { fetchMutation } from "convex/nextjs";
 export async function POST(request: Request) {
   const data = await request.json()
   const transcription = data["transcription"]
-  const scam = false // Query AI/ML model here
+  const response = await fetch("http://127.0.0.1:5000")
+  const response_data = await response.json()
+  console.log(response_data)
+  const scam = response_data["scam"]
+  console.log(scam)
   await fetchMutation(api.transcriptions.createTranscription, { transcription: transcription, scam: scam })
   return Response.json({ success: false, status: scam })
 }
